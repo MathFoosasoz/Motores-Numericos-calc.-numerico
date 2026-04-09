2d43e37b7e27888df9aee0c73ed54c6f8426aecd
CLQueirozz <ceciliaqueiroz@usp.br>
2026-04-09 19:11:28 -0300

    fonte consertada

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import env
 from hydraulics import Hydraulics, Hydraulics_p1, Hydraulics_p2, Hydraulics_p3, Hydraulics_p4, Hydraulics_p5, Hydraulics_p6, complexity_analysis
-from thermal import Thermal
+from thermal import Thermal, Thermal_P2
 from data_structures import GeraGrafo
 from analysis import complexity_analysis
 
@@ -8,10 +8,13 @@ def main():
 
     config_t = env.CONFIG_T
 
-    test_sparse = Thermal(config_t, method = "sparse")
+    #test_sparse = Thermal(config_t, method = "sparse")
     test_cholesky = Thermal(config_t, method = "cholesky")
-    test_sparse.run(print_info = True, plot = True)
+    #test_sparse.run(print_info = True, plot = True)
     test_cholesky.run(print_info = True, plot= True)
+
+    #test_P2 = Thermal_P2(config_t, method="cholesky")
+    #test_P2.run(print_info = True, plot = True)
 
     return
 
--- thermal.py
+++ thermal.py
@@ -182,8 +182,157 @@ class Thermal():
 
 
         if plot:
-            PlotaPlaca(*self.N, *self.L, temps)
+            PlotaPlaca(*self.N, *self.L, temps, filename="img.png")
 
 
+class Thermal_P2(Thermal):
+
+    def __init__(self, config, method = "cholesky"):
+        super().__init__(config)
+
+        circle_dict = config["CIRCULAR_SOURCE_KNOWN_TEMP_DICT"]
+        self.circle_radius = circle_dict["R"]
+        self.circle_coords = circle_dict["coords"]
+        self.circle_temp = circle_dict["T"]
+
+        self.method = method
+
+    @staticmethod
+    def distance(a, b):
+        return ((a[0] - b[0])**2 + (a[1] - b[1])**2)**0.5
+
+    def get_index_inside_circus(self):
+
+        dx = self.L[0]/(self.N[0]-1)
+        dy = self.L[1]/(self.N[1]-1)
+
+        center_x = self.circle_coords[0]*self.L[0]
+        center_y = self.circle_coords[1]*self.L[1]
+
+        center_coords = (center_x, center_y)
+
+        index_inside_circus = []
+        position = []
+
+        for i in range(self.N[1]):
+            for j in range(self.N[0]):
+
+                position = [j*dx, i*dy]
+
+                if self.distance(position, center_coords)<= self.circle_radius:
+                    index_inside_circus.append(self.ij2n(i, j))
+
+        self.index_inside_circus = np.array(index_inside_circus)
+
+        #print(self.index_inside_circus)
+        return
+    
+    def assembly(self):
+        nunk = self.N[0]*self.N[1]
+
+        self.get_index_inside_circus()
+
+        A = np.zeros(shape=(nunk,nunk))
+        b = np.zeros(shape=(nunk,))
+
+        for j in range(1, self.N[0] - 1):
+            for i in range(1, self.N[1] - 1):
+                
+                Ic = self.ij2n(i, j)
+
+                if Ic not in self.index_inside_circus:
+                    Ie = self.ij2n(i+1, j)
+                    Iw = self.ij2n(i-1, j)
+                    In = self.ij2n(i, j+1)
+                    Is = self.ij2n(i, j-1)
 
 
+                    A[Ic,[Ic,Ie,Iw,In,Is]] = 4, -1, -1, -1, -1
+                    b[Ic] = (self.h2 * self.f / self.K)
+
+
+        #print(A)
+        return A, b
+    
+    def solve_system_cholesky(self):
+        nunk = self.N[0]*self.N[1]
+
+        A, b = self.assembly()
+
+        TR, TT, TL, TB = self.temps
+
+        Atilde = A.copy()
+        b_tilde = b.copy()
+
+        kx = np.arange(self.N[0])
+        ky = np.arange(self.N[1])
+
+        #dicionario dos indices das bordas e seus valores pra faciltar
+        boundary={}
+
+        for i in self.index_inside_circus:
+            boundary[i] = self.circle_temp
+
+        for i in kx:
+            boundary[self.ij2n(self.N[1]-1,i)] = TT(i)
+            boundary[self.ij2n(0,i)] = TB(i)
+
+        for i in ky:
+            boundary[self.ij2n(i,self.N[0]-1)] = TR
+            boundary[self.ij2n(i,0)] = TL
+
+        # PEGUE Atilde AQ (PROBLEMA DIFÍCIL 1)!!! 
+
+        for index, value in boundary.items():
+            for i in range(nunk):
+                if i != index:
+                    b_tilde[i] -= A[i, index]*value
+
+                    Atilde[i, index] = 0
+                    Atilde[index, i] = 0
+
+            Atilde[index, index] = 1
+            b_tilde[index] = value
+
+        #aplicação do método cholesky 
+        try: 
+            L = np.linalg.cholesky(Atilde)
+        except np.linalg.LinAlgError:
+            print("erro na montagem da matriz (não positiva ou não simétrica)")
+            return None
+        
+        y = np.linalg.solve(L, b_tilde)
+    
+        Temperature = np.linalg.solve(L.T, y)
+        return Temperature
+    
+    def run(self, print_info = False, plot = False):
+
+        if self.method == "cholesky":
+            temps = self.solve_system_cholesky()
+
+        elif self.method == "sparse":
+            temps = self.solve_system_sparse()
+
+        else:
+            raise ValueError()
+
+        if print_info:
+            print(f"Resultados para classe: {self.__class__.__name__}")
+            print(f"Resolvido por: {self.method}")
+            print(f"solução das temperaturas do sistema:")
+            
+            if self.N[0]>21:
+                with open("example.txt", "a") as file:
+                    for i in range(self.N[1]-1, -1, -1):
+                        for j in range(self.N[0]):
+                            global_index = self.ij2n(i,j)
+                            t = self.round_up_to_nearest_00_1(temps[global_index])
+                            file.write(f"{t:.2f}  ")
+
+
+                        file.write("\n")
+
+
+        if plot:
+            PlotaPlaca(*self.N, *self.L, temps, filename="img.png")
