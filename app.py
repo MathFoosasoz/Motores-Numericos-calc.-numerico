8ba68902e37ec7c001bc451ca6d66724a789c9fb
victorkoeche <victorkoechesilva@gmail.com>
2026-06-06 23:05:57 -0300

    plotagens problema 2

--- app.py
+++ app.py
@@ -1,26 +1,73 @@
 import env
 from hydraulics import Hydraulics, Hydraulics_p1, Hydraulics_p2, Hydraulics_p3, Hydraulics_p4, Hydraulics_p5, Hydraulics_p6, complexity_analysis
-from thermal import Thermal, Thermal_P1, Thermal_P2, Thermal_P3, Thermal_P4, Thermal_P5, Thermal_P1_extra, Thermal_P2_Extra
-from mechanic import Mechanic, Mechanic_P2, Mechanic_P4
-from mechanic_hydraulic import MechanicHydraulic
-from hydraulic_thermal import Hydraulic_to_Thermal
-from hydraulic_thermal import Hydraulic_to_Thermal_P2
-from hydraulic_thermal import Hydraulic_to_Thermal_P2_circulo
-from themal_hydraulics import HydraulicThermal
+from thermal import Thermal, Thermal_P1, Thermal_P2, Thermal_P3, Thermal_P4, Thermal_P5, Thermal_P1_extra, Thermal_P2_Extra, Thermal_P3_Extra
+from mechanic_hydraulic import Mechanic_Hydraulic
 from data_structures import GeraGrafo
 from analysis import complexity_analysis
 
-
 def main():
 
-    config_mh = env.CONFIG_MH
+    config_t = env.CONFIG_T
 
-    test = MechanicHydraulic(config_mh)
-    test.run(print_info=True, plot=False)
+    #FUNCIONA!!!
+    #test_sparse = Thermal(config_t, method = "sparse")
+    #test_sparse.run(print_info = True, plot = True)
+
+    #FUNCIONA!!!
+    #test_cholesky = Thermal(config_t, method = "cholesky")
+    #test_cholesky.run(print_info = True, plot= True)
+
+    #FUNCIONA!!! 
+    #test_P1 = Thermal_P1(config_t, method="sparse")
+    #test_P1.complexity_analysis(print_info=True, plot=True)
+    #test_P1.run(print_info = True, plot = True)
+    
+    #FUNCIONA!!!
+    #test_P2 = Thermal_P2(config_t, method="sparse")
+    #test_P2.run(print_info = False, plot = True)
+
+    #FUNCIONA!!!
+    #test_P3 = Thermal_P3(config_t, method="sparse")
+    #test_P3.run(print_info = False, plot = True)
+
+    #FUNCIONA!!!
+    #test_P4 = Thermal_P4(config_t)
+    #test_P4.run(plot=True)
+
+    #FUNCIONA!!!
+    #test_P5 = Thermal_P5(config_t, k_node=233)
+    #test_P5.run(print_info=True)
+    
+    #FUNCIONA !!!
+    #test_jacobi = Thermal_P1_extra(config_t, method = "jacobi")
+    #test_gauss_seidel = Thermal_P1_extra(config_t, method = "gauss_seidel")
+    #test_jacobi.run(print_info = True, plot = True)
+    #test_gauss_seidel.run(print_info = True, plot = True)
+    
+    #test_gauss_seidel.run(print_info = True, plot = True, analyze_subdivisions=True, analyze_tolerance=True)
+
+    #FUNCIONA !!!
+    #gsimulacao = Thermal_P2_Extra(config_t)
+    #res_j, res_gs = simulacao.run_comparison_history(frame_step=20)
+    #simulacao.animate_comparison(interval=50)
+
+    #FUNCIONA !!!
+    #test_P3_extra = Thermal_P3_Extra(config_t)
+    #test_P3_extra.run(print_info = True, plot=False)
+
+    simulador = MechanicHydraulic(config_projeto)
+
+    print("Iniciando a varredura transiente de todos os cenários... (Aguarde)")
+
+    todos_resultados = simulador.resolver_todos_cenarios(print_info=False)
+
+    print("Simulações finalizadas. Gerando arquivos de plotagem...")
+
+    gerar_todos_os_plots(todos_resultados)
+    print("Gráficos salvos")
     
     return
 
 
 if __name__ == "__main__":
     main()
-    
